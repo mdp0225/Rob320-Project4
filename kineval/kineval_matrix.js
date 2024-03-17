dc7d4c2c5a41f4b5f05aec447c7087c4b523e44d
Matt Prince <115430383+mdp0225@users.noreply.github.com>
2024-03-17 19:10:21 -0400

    Update kineval_matrix.js

--- kineval/kineval_matrix.js
+++ kineval/kineval_matrix.js
@@ -36,16 +36,34 @@ function matrix_copy(m1) {
 // **** Function stencils are provided below, please uncomment and implement them ****//
 
 
+function  matrix_multiply(m1,m2){
+    // returns 2D array that is the result of m1*m2
+    var m = new Array(m1.length);
+    for (i=0;i<m1.length;++i){
+        m[i] = new Array(m2[0].length);
+        for (j=0;j<m2[0].length;++j){
+            var val = 0;
+            for (k=0;k<m2.length;++k){
+                val = val + m1[i][k]*m2[k][j];
 
-// function matrix_multiply(m1,m2) {
-//     // returns 2D array that is the result of m1*m2
+            }
+            m[i][j]=val;
+        }
+    }
+    return m;
+}
 
-// }
-
-// function matrix_transpose(m) {
-//     // returns 2D array that is the result of m1*m2
-
-// }
+function matrix_transpose(m) {
+    // returns 2D array that is the result of m1*m2  <--- don't know if this comment is correct?
+    var result = [];
+    for (var i = 0; i < m[0].length; i++) {
+        result[i] = [];
+        for (var j = 0; j < m.length; j++) {
+            result[i][j] = m[j][i];
+        }
+    }
+    return result;
+}
 
 // function matrix_pseudoinverse(m) {
 //     // returns pseudoinverse of matrix m
@@ -57,37 +75,80 @@ function matrix_copy(m1) {
 
 // }
 
-// function vector_normalize(v) {
-//     // returns normalized vector for v
-    
-// }
+function vector_normalize(v) {
+    // returns normalized vector for v
+    var divisor = 0;
+    for (var i = 0; i < v.length; i++) {
+        divisor += v[i] * v[i];
+    }
+    divisor = Math.sqrt(divisor);
+    for (var i = 0; i < v.length; i++) {
+        v[i] = v[i] / divisor;
+    }
+    return v;
+}
 
-// function vector_cross(a,b) {
-//     // return cross product of vector a and b with both has 3 dimensions
-    
-// }
+function vector_cross(a,b) {
+    // return cross product of vector a and b with both has 3 dimensions
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    ];
+}
 
-// function generate_identity() {
-//     // returns 4-by-4 2D array of identity matrix
-    
-// }
+function generate_identity() {
+    // returns 4-by-4 2D array of identity matrix
+    return [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ];
+}
 
-// function generate_translation_matrix(tx, ty, tz) {
-//     // returns 4-by-4 matrix as a 2D array
-    
-// }
+function generate_translation_matrix(tx, ty, tz) {
+    // returns 4-by-4 matrix as a 2D array
+    return [
+        [1, 0, 0, tx],
+        [0, 1, 0, ty],
+        [0, 0, 1, tz],
+        [0, 0, 0, 1]
+    ];
+}
 
-// function generate_rotation_matrix_X(angle) {
-//     // returns 4-by-4 matrix as a 2D array, angle is in radians
-    
-// }
+function generate_rotation_matrix_X(angle) {
+    // returns 4-by-4 matrix as a 2D array, angle is in radians
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+    return [
+        [1, 0, 0, 0],
+        [0, cos, -sin, 0],
+        [0, sin, cos, 0],
+        [0, 0, 0, 1]
+    ];
+}
 
-// function generate_rotation_matrix_Y(angle) {
-//     // returns 4-by-4 matrix as a 2D array, angle is in radians
-    
-// }
+function generate_rotation_matrix_Y(angle) {
+    // returns 4-by-4 matrix as a 2D array, angle is in radians
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+    return [
+        [cos, 0, sin, 0],
+        [0, 1, 0, 0],
+        [-sin, 0, cos, 0],
+        [0, 0, 0, 1]
+    ];
+}
 
-// function generate_rotation_matrix_Z(angle) {
-//     // returns 4-by-4 matrix as a 2D array, angle is in radians
-    
-// }
+function generate_rotation_matrix_Z(angle) {
+    // returns 4-by-4 matrix as a 2D array, angle is in radians
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+    return [
+        [cos, -sin, 0, 0],
+        [sin, cos, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ];
+}
